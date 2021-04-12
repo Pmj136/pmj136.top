03dfde9e4a9994465913a25f43a319ce37a8fdb1
PengMingJiu <pengmingjiu@gmail.com>
2021-04-12 21:54:17 +0800

    删除投喂

--- src/views/article/components/Advertising.vue
+++ src/views/article/components/Advertising.vue
@@ -1,58 +1,58 @@
 <template>
-  <div class="jiu-card">
-    <div class="jiu-card-header">
-      <span class="jiu-card-header--text">技术支持</span>
+    <div class="jiu-card">
+        <div class="jiu-card-header">
+            <span class="jiu-card-header--text">技术支持</span>
+        </div>
+        <div class="container">
+            <div class="qq" @click="toQQ">
+                QQ:1367559786
+            </div>
+            <div class="mine">
+                Wx:Sweet_case
+            </div>
+        </div>
     </div>
-    <div class="container">
-      <div class="qq" @click="toQQ">
-        QQ:1367559786
-      </div>
-      <div class="mine">
-        pengmingjiu@gmail.com
-      </div>
-    </div>
-  </div>
 </template>
 <script>
 export default {
-  methods: {
-    toQQ() {
-      window.location.href = 'tencent://message/?uin=1367559786'
-    },
-  }
+    methods: {
+        toQQ() {
+            window.location.href = 'tencent://message/?uin=1367559786'
+        },
+    }
 }
 </script>
 <style scoped lang="scss">
 .jiu-title {
-  border-left-color: #ac1066;
+    border-left-color: #ac1066;
 }
 
 .container {
-  padding: 10px;
-  box-sizing: border-box;
+    padding: 10px;
+    box-sizing: border-box;
 }
 
 .qq, .mine {
-  cursor: pointer;
-  padding: 14px 0;
-  box-sizing: border-box;
-  color: #f1f0f0;
-  text-align: center;
+    cursor: pointer;
+    padding: 14px 0;
+    box-sizing: border-box;
+    color: #f1f0f0;
+    text-align: center;
 }
 
 .qq {
-  background-color: #708ebd;
-  user-select: none;
+    background-color: #708ebd;
+    user-select: none;
 }
 
 .qq:hover, .mine:hover {
-  opacity: 0.8;
+    opacity: 0.8;
 }
 
 .mine {
-  background-color: #47a16e;
-  margin-top: 10px;
-  cursor: text;
+    background-color: #47a16e;
+    margin-top: 10px;
+    cursor: text;
 }
 
 </style>
--- src/views/feed/index.vue
+++ src/views/feed/index.vue
@@ -1,43 +1,40 @@
 <template>
-  <section class="jiu-page--feed">
-    <jiu-react-box full>
-      <div class="jiu-card">
-        <a class="link" href="https://gitee.com/MrPengMJ/pmj136.top" target="_blank">前端代码开源地址</a>
-        <el-image
-            class="feed-img"
-            :src="wx" fit="cover"
-            :preview-src-list="[wx]"/>
-      </div>
-    </jiu-react-box>
-  </section>
+    <section class="jiu-page--feed">
+        <jiu-react-box full>
+            <div class="jiu-card">
+                <div style="padding: 100px">
+                    有意合作者，联系首页QQ
+                    <p>或邮箱：<a href="mailto:pengmingjiu@gmail.com">pengmingjiu@gmail.com</a> or <a
+                        href="mailto:pengmingjiu@aliyun.com">pengmingjiu@aliyun.com</a></p>
+                </div>
+            </div>
+        </jiu-react-box>
+    </section>
 </template>
 <script>
 import JiuReactBox from "@/components/JiuReactBox"
-import wx from "@/assets/wx.png"
 
 export default {
-  components: {JiuReactBox},
-  data() {
-    return {
-      wx,
+    components: {JiuReactBox},
+    data() {
+        return {}
     }
-  }
 }
 </script>
 <style lang="scss" scoped>
 .jiu-card {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
 
 .link {
-  font-size: 1.6rem;
-  margin: 20px 0;
+    font-size: 1.6rem;
+    margin: 20px 0;
 }
 
 .feed-img {
-  width: 200px;
+    width: 200px;
 }
 </style>
